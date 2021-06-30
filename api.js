f9fe0bb6b484f779302ede83bb7ad06fc47ef8ef
W3bwizard <w3bwiz4rd@gmail.com>
2021-06-30 23:37:27 +0300

    fix state

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
-window.onpopstate = function(event){
-  console.log(JSON.stringify(event.state))
-}
+window.onpopstate = function(event) {
+  console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+};
+history.pushState({page: 1}, "title 1", "?page=1");
+history.pushState({page: 2}, "title 2", "?page=2");
+history.replaceState({page: 3}, "title 3", "?page=3");
